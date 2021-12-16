a2d50bcb7f357c127699264ee998b16d16636a77
kenke11 <kenkebashvili2@gmail.com>
2021-12-16 12:07:44 +0400

    chore: install php-cs-fixer

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,8 @@
         <div class="flex-1 flex flex-col w-7/12 justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
             <div class="mx-auto w-full max-w-sm lg:w-96">
                 <div>
-                    <img class="h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow">
-                    <h2 class="mt-6 text-3xl font-semibold text-gray-900">
+                    <img class="h-12 w-auto" src="{{asset('images/logo.png')}}" alt="Workflow">
+                    <h2 class="mt-16 text-3xl font-semibold text-gray-900">
                         Welcome back
                     </h2>
                     <p class="mt-4 text-sm text-gray-500">
@@ -82,7 +82,7 @@
             </div>
         </div>
         <div class="hidden lg:block relative w-0 flex-1">
-            <img class="absolute inset-0 h-full w-full object-cover" src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80" alt="">
+            <img class="absolute inset-0 h-full w-full object-cover" src="{{asset('images/covid-vaccine.png')}}" alt="">
         </div>
     </div>
 
