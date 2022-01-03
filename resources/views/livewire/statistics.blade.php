55f604a86b8a812b10958c970f5002bee591ee22
kenke11 <kenkebashvili2@gmail.com>
2022-01-03 14:46:58 +0400

    style: change search type

--- resources/views/livewire/statistics.blade.php
+++ resources/views/livewire/statistics.blade.php
@@ -8,7 +8,7 @@
         </div>
         <input
             wire:model="search"
-            type="search"
+            type="text"
             name="search"
             id="search"
             class="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-14 py-4 sm:text-sm border-gray-300 rounded-md"
