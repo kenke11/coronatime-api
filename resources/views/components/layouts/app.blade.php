b65025493cdaa2b08d60129776984cf4aa86f115
kenke11 <kenkebashvili2@gmail.com>
2022-01-05 13:28:21 +0400

    style: change logo

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -5,9 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{assert('images/icon.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/icon.png')}}">
     <title>Coronatime</title>
+    <link rel="icon" type="image/png" href="{{asset('images/logo.png')}}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
     @livewireStyles
--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{assert('images/icon.png')}}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('images/icon.png')}}">
+    <link rel="icon" type="image/png" href="{{assert('images/logo.png')}}">
     <title>Coronatime</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ asset('js/app.js') }}" defer></script>
