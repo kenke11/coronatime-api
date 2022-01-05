ab59f531cfed3d27de59e450dfa11f0ac64fa57b
kenke11 <kenkebashvili2@gmail.com>
2022-01-05 19:00:13 +0400

    fix: georgian validation

--- resources/lang/ka/validation.php
+++ resources/lang/ka/validation.php
@@ -4,8 +4,8 @@ return [
 	'required'  => ':attribute ველი აუცილებელია.',
 	'min'       => [
 		'numeric' => ':attribute უნდა იყოს მინიმუმ :min.',
-		'string'  => ':attribute ინდა იყოს მინიმუმ :min სიმბოლო.',
+		'string'  => ':attribute უნდა იყოს მინიმუმ :min სიმბოლო.',
 	],
 	'unique'           => 'ასეთი :attribute უკვე არსებობს.',
-	'email'            => ':attribute უნდა იყოს ელ-ფოსტა.',
+	'email'            => ':attribute -ს ველი უნდა იყოს ელ-ფოსტა.',
 ];
