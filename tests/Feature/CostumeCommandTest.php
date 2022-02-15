92ea9c81fb3d697e60e87019cf21fa974821ef6c
kenke11 <kenkebashvili2@gmail.com>
2022-02-15 21:07:52 +0400

    fix: corected api patch with test

--- tests/Feature/CostumeCommandTest.php
+++ tests/Feature/CostumeCommandTest.php
@@ -16,20 +16,20 @@ class CostumeCommandTest extends TestCase
 	public function get_countries_and_post_them_with_database()
 	{
 		Http::fake([
-			'https://devtest.ka/countries' => Http::response(
+			'https://devtest.ge/countries' => Http::response(
 				json_decode(file_get_contents('tests/data-json/get-request.json'), true),
 				200,
 			),
 		]);
 
 		Http::fake([
-			'https://devtest.ka/get-country-statistics' => Http::response(
+			'https://devtest.ge/get-country-statistics' => Http::response(
 				json_decode(file_get_contents('tests/data-json/post-request.json'), true),
 				200,
 			),
 		]);
 
-		$this->artisan('command:update-statistics');
+		$this->artisan('update:statistics');
 
 		$this->assertDatabaseHas('countries', [
 			'code' => 'GE',
