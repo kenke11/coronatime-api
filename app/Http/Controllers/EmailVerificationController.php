555c845d133d437f630ee61cb7bd24e396ec1764
kenke11 <kenkebashvili2@gmail.com>
2022-02-24 19:58:46 +0400

    fix: change login ling to production

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -18,18 +18,16 @@ class EmailVerificationController extends Controller
 			{
 				$user->email_verified_at = Carbon::now();
 				$user->save();
-//				return redirect()->route('login')->with('success', 'Your email has been verified!');
-				return redirect('http://localhost:3000/login');
+				return redirect('https://coronatime.tazo.redberryinternship.ge/login');
 			}
 			else
 			{
-//				return redirect()->route('login')->with('info', 'Your email has already been verified');
-				return redirect('http://localhost:3000/login');
+				return redirect('https://coronatime.tazo.redberryinternship.ge/login');
 			}
 		}
 		else
 		{
-			redirect()->route('login')->with('error', 'Something wend wrong!');
+			return redirect('https://coronatime.tazo.redberryinternship.ge/login')->with('error', 'Something wend wrong!');
 		}
 	}
 }
