be53f167b4e061f1bd51def534500cd29af3aee6
kenke11 <kenkebashvili2@gmail.com>
2022-03-05 15:45:32 +0400

    refactor: email verification contorller

--- app/Http/Controllers/EmailVerificationController.php
+++ app/Http/Controllers/EmailVerificationController.php
@@ -9,25 +9,19 @@ class EmailVerificationController extends Controller
 {
 	public function verifyEmail($token)
 	{
-		$verifiedUser = User::where('email_verified_token', $token)->first();
+		$user = User::where('email_verified_token', $token)->first();
 		if (isset($verifiedUser))
 		{
-			$user = $verifiedUser;
-
 			if (!$user->email_verified_at)
 			{
 				$user->email_verified_at = Carbon::now();
 				$user->save();
 				return redirect('https://coronatime.tazo.redberryinternship.ge/login');
 			}
-			else
-			{
-				return redirect('https://coronatime.tazo.redberryinternship.ge/login');
-			}
+
+			return redirect('https://coronatime.tazo.redberryinternship.ge/login');
 		}
-		else
-		{
-			return redirect('https://coronatime.tazo.redberryinternship.ge/login')->with('error', 'Something wend wrong!');
-		}
+
+		return redirect('https://coronatime.tazo.redberryinternship.ge/login');
 	}
 }
